53f7a1ab3368e73aa1f1206a2472269d61a92404
Cyberflixt <54700008+Cyberflixt@users.noreply.github.com>
2023-11-29 14:01:37 +0100

    com

--- libs/puissance4_Bypass.py
+++ libs/puissance4_Bypass.py
@@ -1,21 +1,26 @@
 
-"""
-Alternative à selenium afin de bypass le wifi de l'école
-Utilise des get requests avec les information en json
-passer dans l'url demandé
-"""
-
 from selenium import webdriver
 from selenium_stealth import stealth
 import time
 import json
 
+"""
+Alternative à selenium afin de bypass le wifi de l'école
+Utilise des get requests avec les information en json
+passer dans l'url demandé
+
+Selenium permet d'ouvrir Chromium driver et d'entrer une url
+Et d'éxécuter du javascript
+"""
+
 class Resultat:
     def __init__(self, data):
+        """Object imitant le résultat d'une requête à partir d'un texte"""
         self.data = data
         self.status_code = 200
 
     def json(self):
+        """Renvoie les données json en dictionnaires"""
         return json.loads(self.data)
 
     def __str__(self):
@@ -25,6 +30,7 @@ class Resultat:
 
 class Bypass:
     def __init__(self, **kwargs):
+        """Classe imitant le module requests mais en utilisant Chromium driver"""
         self.driver = None
         self.starting = False
         self.headless = False
@@ -49,7 +55,7 @@ class Bypass:
             print('Webdriver headless')
             options.add_argument('--headless')
 
-        print(f'Chargement du webdriver (headless = {self.headless})')
+        print(f'Chargement du webdriver (headless = {self.headless})\nVeuillez patienter...')
 
         self.driver = webdriver.Chrome(options=options)
         print('Webdriver chargé')
@@ -67,7 +73,7 @@ class Bypass:
     def get(self, url):
         """Obtention du texte d'une page"""
         # On fait charger la page internet
-        self.start()
+        self.start() # Initialise le driver si ce n'est pas déjà fait
         self.driver.get(url)
         
         # Attendre que la page charge
